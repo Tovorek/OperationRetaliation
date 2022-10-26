a699532b13e8a4d0ebf4ef39ac61762aef92d292
Arrow <71452112+buhuhaha@users.noreply.github.com>
2022-10-25 21:47:46 -0400

    Tournament Winner

---
+++ Hela's Game.py
@@ -0,0 +1,19 @@
+HOME_TEAM_WON = 1
+# O(n) time | O(k) space
+def tournamentWinner(competitions, results):
+    currentBestTeam = ""
+    scores = {currentBestTeam: 0}
+
+    for idx, competition in enumerate(competitions):
+        result = results[idx]
+        homeTeam, awayTeam = competition
+
+        winningTeam = homeTeam if result == HOME_TEAM_WON else awayTeam
+
+        updateScores(winningTeam, 3, scores)
+
+        if scores[winningTeam] > scores[currentBestTeam]:
+            currentBestTeam = winningTeam
+
+    return currentBestTeam
+  
